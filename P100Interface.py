6b7f54f053ce3ecdac8bd0a021821840b9062f6b
George Vettraino <gj.vettraino@gmail.com>
2022-01-12 21:26:55 +0000

    Added README and comments to code

--- P100Interface.py
+++ P100Interface.py
@@ -4,11 +4,13 @@ import configparser
 import sys
 import time
 
+# Reads the config.ini file to get IP, email and password 
 def readConfig() -> tuple[str,str,str]:
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config["DEFAULT"]["IP"], config["DEFAULT"]["EMAIL"], config["DEFAULT"]["PASSWORD"]
 
+# Authenticates with the plug to allow us to control it
 def connect() -> tapo.P100:
     ip, email, password = readConfig()
     plug = tapo.P100(ip,email,password)
@@ -16,18 +18,21 @@ def connect() -> tapo.P100:
     plug.login()
     return plug
 
-def status(plug:tapo.P100):
+# Displays info about the plug
+def info(plug:tapo.P100):
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(plug.getDeviceInfo())
     code = plug.getDeviceInfo().get("error_code")
     meaning = plug.errorCodes.get(str(code))
     print(f"Error Code {code}: {meaning}")
 
+# An experimental timer function that turns the plug off after a specified time
 def timer(plug:tapo.P100,minutes:int):
     plug.turnOn()
     time.sleep(minutes*60)
     plug.turnOff()
 
+# Displays the help message explaining each argument
 def help():
     with open("docs/P100Interface.txt") as f:
         instructions = f.read()
@@ -44,8 +49,8 @@ if (__name__ == "__main__"):
     elif (sys.argv[1] == "off"):
         plug.turnOff()
         print(f"{plug.getDeviceName()}: OFF")
-    elif (sys.argv[1] == "status"):
-        status(plug)
+    elif (sys.argv[1] == "info"):
+        info(plug)
     elif (sys.argv[1] == "name"):
         print(f"Device Name: {plug.getDeviceName()}")
     elif (sys.argv[1] == "timer" and len(sys.argv) > 2):
---
+++ README.md
@@ -0,0 +1,35 @@
+# Tapo P100 Controller
+
+A collection of Python scripts for controlling a Tapo P100 smart plug. A lot of the backend is modified code from this [repo](https://github.com/fishbigger/TapoP100). The main way to interact with these libraries is to run the `P100Interface.py` file with any of the supported command line options:
+
+```
+Command Line Tool for Controlling a Tapo P100 Plug
+
+Options:
+    on: turn the plug on
+    off: turn the plug off
+    info: get information about the current state of the plug
+    name: get the name of the plug
+    timer [minutes]: turn the plug on for the specified number of minutes
+    help: display this message
+```
+
+The above message can be displayed by running the file with no arguments or the `help` argument.
+
+# First Use
+
+Before running the `P100Interface.py` script, modify the `config.ini` file to contain the IP of the smart plug and email address and password for the account associated with the device:
+
+```ini
+[DEFAULT]
+IP = 192.168.0.100
+EMAIL = example@email.com
+PASSWORD = top_secret
+```
+
+# Dependencies
+
+```bash
+python3 -m pip install pycryptodome pkcs7
+```
+
